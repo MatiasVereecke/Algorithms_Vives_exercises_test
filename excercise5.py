6e21bb8c4b2d86bd88612cd83fad82c4aa4f1488
Matias Vereecke <matias.vereecke@student.vives.be>
2021-10-13 15:43:22 +0200

    adding files

--- excercise5.py
+++ excercise5.py
@@ -5,8 +5,8 @@ def riemannSum(a,b,n):
   deelint = (b-a)/n
   for i in range(1,n):
     funct += ((a+(i*deelint))**3)
-    funct += b**3
+  funct += (b**3)
 
   return deelint * funct
 
-print(riemannSum(2,3,4))
+print(riemannSum(2,9,8))
